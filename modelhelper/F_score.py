3cdcc5069d32d4166b55d42ee164b339594bcc7f
Shterenberg <jshteren@vlsi24.bu.edu>
2022-05-05 18:42:19 -0400

    Updated Scratchbook, added F-score

--- modelhelper/F_score.py
+++ modelhelper/F_score.py
@@ -1,21 +1,24 @@
 from sklearn.metrics import confusion_matrix
 import seaborn as sn
 import pandas as pd
+import torch
 
 def F_score(net, inputs, labels):
-	y_pred = []
-	y_test = []
-	
-	output = net(inputs)
-	output = torch.argmax(output).cpu().numpy()
-	y_pred.extend(output)
-	
-	labels = labels.cpu().numpy()
-	y_true.extend(labels)
+    y_pred = []
+    y_true = []
 
-	classes = ('1 Star', '2 Stars', '3 Stars', '4 Stars', '5 Stars')
-	cf_matrix = confusion_matrix(y_true, y_pred)
-	df_cm = pd.Dataframe(cf_matrix/np.sum(cf_matrix)*10, index=[i for i in classes], columns = [i for i in classes])
-	plt.figure(figsize = (12,7))
-	sn.heatmap(df_cm, annot=True)
-	plt.savefig('output.png')
+    output = net(inputs)
+    output = torch.argmax(output, dim=1).cpu().numpy()
+    y_pred.extend(output)
+    
+    y_true.extend(labels.cpu().numpy())
+    
+    classes = ('1 Star', '2 Stars', '3 Stars', '4 Stars', '5 Stars')
+    cf_matrix = confusion_matrix(y_true, y_pred)
+    #print(cf_matrix)
+    df_cm = pd.DataFrame(cf_matrix/np.sum(cf_matrix)*10, index=[i for i in classes], columns = [i for i in classes])
+    plt.figure(figsize = (12,7))
+    plt.xlabel("Predicted Label")
+    plt.ylabel("True Label")
+    sn.heatmap(df_cm, annot=True)
+    plt.savefig('output.png')
